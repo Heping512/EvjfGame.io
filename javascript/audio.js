f45234c0db9c6b2a718442df58975df867dc9d05
Heping512 <kristyhuang.51200@gmail.com>
2025-05-24 18:04:04 +0200

    Deploiement1

---
+++ javascript/audio.js
@@ -0,0 +1,24 @@
+﻿window.quizAudio = {
+    playTicTac: function () {
+        const audio = document.getElementById("audio-tictac");
+        if (audio) {
+            audio.loop = true;
+            audio.currentTime = 0;
+            audio.play();
+        }
+    },
+    stopTicTac: function () {
+        const audio = document.getElementById("audio-tictac");
+        if (audio) {
+            audio.pause();
+            audio.currentTime = 0;
+        }
+    },
+    playDong: function () {
+        const audio = document.getElementById("audio-dong");
+        if (audio) {
+            audio.currentTime = 0;
+            audio.play();
+        }
+    }
+};
